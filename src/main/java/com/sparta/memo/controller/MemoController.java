2c22149380ae534e0e03cbe6699e5b9f434ee631
Thomas Sang <40707686+uiseongsang@users.noreply.github.com>
2023-06-14 20:46:35 +0900

    Duplicate Code Handling with IoC & DI

--- src/main/java/com/sparta/memo/controller/MemoController.java
+++ src/main/java/com/sparta/memo/controller/MemoController.java
@@ -11,35 +11,30 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class MemoController {
-    private final JdbcTemplate jdbcTemplate;
+    private final MemoService memoService;
 
     public MemoController(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
+        this.memoService = new MemoService(jdbcTemplate);
     }
 
     @PostMapping("/memos")
     public MemoResponseDto createMemo(@RequestBody MemoRequestDto requestDto) {
-        // 객체 간 이동을 할 떄 즉 다른 class의 method를 호출하려면 그 class를 객체를 만들어야함(instance화)
-        MemoService memoService = new MemoService(jdbcTemplate);
         // Conteroller method와 Service method 일치를 해줘야지 안 헷갈림
         return memoService.createMemo(requestDto);
     }
 
     @GetMapping("/memos")
     public List<MemoResponseDto> getMemos() {
-        MemoService memoService = new MemoService(jdbcTemplate);
         return memoService.getMemos();
     }
 
     @PutMapping("/memos/{id}")
     public Long updateMemo(@PathVariable Long id, @RequestBody MemoRequestDto requestDto) {
-        MemoService memoService = new MemoService(jdbcTemplate);
         return memoService.updateMemo(id, requestDto);
     }
 
     @DeleteMapping("/memos/{id}")
     public Long deleteMemo(@PathVariable Long id) {
-        MemoService memoService = new MemoService(jdbcTemplate);
         return memoService.deleteMemo(id);
     }
 }
