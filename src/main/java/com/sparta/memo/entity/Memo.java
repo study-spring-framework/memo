c2b7f04109ea8e39d5cf8aff64a1a4fb7c119a4b
Thomas Sang <40707686+uiseongsang@users.noreply.github.com>
2023-06-14 15:32:52 +0900

    Implement Create, Read API

---
+++ src/main/java/com/sparta/memo/entity/Memo.java
@@ -0,0 +1,21 @@
+package com.sparta.memo.entity;
+
+import com.sparta.memo.dto.MemoRequestDto;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class Memo {
+    private Long id; // 메모끼리 구분
+    private String username;
+    private String contents;
+
+    public Memo(MemoRequestDto requestDto) {
+        // 클라이언트에서 받아온 데이터가 requestDto에 들어가 있다
+        this.username = requestDto.getUsername();
+        this.contents = requestDto.getContents();
+    }
+}
