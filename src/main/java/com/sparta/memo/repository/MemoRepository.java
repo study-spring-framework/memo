907a0bebfdc23e8e9c98bf3aef83a3ad21949519
Thomas Sang <40707686+uiseongsang@users.noreply.github.com>
2023-06-16 11:39:19 +0900

    What is Query Method?

--- src/main/java/com/sparta/memo/repository/MemoRepository.java
+++ src/main/java/com/sparta/memo/repository/MemoRepository.java
@@ -2,6 +2,9 @@ package com.sparta.memo.repository;
 
 import com.sparta.memo.entity.Memo;
 import org.springframework.data.jpa.repository.JpaRepository;
-public interface MemoRepository extends JpaRepository<Memo, Long> {
 
+import java.util.List;
+
+public interface MemoRepository extends JpaRepository<Memo, Long> {
+    List<Memo> findAllByOrderByModifiedAtDesc();
 }
