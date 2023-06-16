aaee6ad183208cd48d70479b1926fa0e267989b5
Thomas Sang <40707686+uiseongsang@users.noreply.github.com>
2023-06-16 10:15:32 +0900

    What is Spring Data JPA?

--- src/main/java/com/sparta/memo/service/MemoService.java
+++ src/main/java/com/sparta/memo/service/MemoService.java
@@ -5,6 +5,7 @@ import com.sparta.memo.dto.MemoResponseDto;
 import com.sparta.memo.entity.Memo;
 import com.sparta.memo.repository.MemoRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 @Service
@@ -19,7 +20,7 @@ public class MemoService {
         // RequestDto -> Entity
         Memo memo = new Memo(requestDto);
 
-        // DB 저장
+        // DB 저장 - SimpleJpaRepository의 save 메소드를 사용
         Memo saveMemo = memoRepository.save(memo);
 
         // Entity -> ResponseDto
@@ -29,16 +30,20 @@ public class MemoService {
     }
 
     public List<MemoResponseDto> getMemos() {
+        // MemoResponseDto 타입이라서 한 번 변환해준다
+
         // DB 조회
-        return memoRepository.findAll();
+        return memoRepository.findAll().stream().map(MemoResponseDto::new).toList();
     }
 
+    @Transactional
     public Long updateMemo(Long id, MemoRequestDto requestDto) {
         // 해당 메모가 DB에 존재하는지 확인
-        Memo memo = memoRepository.findById(id);
+        Memo memo = findMemo(id);
+
         if(memo != null) {
             // memo 내용 수정
-            memoRepository.update(id, requestDto);
+            memo.update(requestDto);
 
             return id;
         } else {
@@ -48,15 +53,18 @@ public class MemoService {
 
     public Long deleteMemo(Long id) {
         // 해당 메모가 DB에 존재하는지 확인
-        Memo memo = memoRepository.findById(id);
-        if(memo != null) {
-            // memo 삭제
-            memoRepository.delete(id);
+        Memo memo = findMemo(id);
 
-            return id;
-        } else {
-            throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
-        }
+        // memo 삭제
+        memoRepository.delete(memo);
+
+        return id;
     }
 
+    private Memo findMemo(Long id) {
+        // Optional이라서 null 체크해줘야함
+        return memoRepository.findById(id).orElseThrow(() ->
+                new IllegalArgumentException("선택한 메모는 존재하지 않습니다")
+        );
+    }
 }
