650c534f99257750de293aaf019cb741f68c8890
Thomas Sang <40707686+uiseongsang@users.noreply.github.com>
2023-06-16 11:17:10 +0900

    Apply JPA Auditing

--- src/main/java/com/sparta/memo/dto/MemoResponseDto.java
+++ src/main/java/com/sparta/memo/dto/MemoResponseDto.java
@@ -10,11 +10,15 @@ public class MemoResponseDto {
     private Long id;
     private String username;
     private String contents;
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
 
     public MemoResponseDto(Memo memo) {
         this.id = memo.getId();
         this.username = memo.getUsername();
         this.contents = memo.getContents();
+        this.createdAt = memo.getCreatedAt();
+        this.modifiedAt = memo.getModifiedAt();
     }
 
 }
